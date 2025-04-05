bf0ce79c4659d0623095951d7f961ea73846e8a9
Aashutosh Sharma <aashugfx@gmail.com>
2025-04-05 22:57:06 +0530

    testing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -17,57 +16,19 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 20
   },
-  email: {
+  avatarId: {
     type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  profilePicture: {
-    type: String,
-    default: null
+    required: true  // assuming all users must select one of your premade avatars
   },
   stats: {
     gamesPlayed: { type: Number, default: 0 },
-    bestLevel: { type: Number, default: 1 },
-    totalWins: { type: Number, default: 0 },
-    totalAttempts: { type: Number, default: 0 }
+    bestLevel: { type: Number, default: 1 }
   },
   createdAt: {
     type: Date,
     default: Date.now
-  },
-  lastLoginAt: {
-    type: Date,
-    default: Date.now
   }
 });
-
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Method to compare password
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
